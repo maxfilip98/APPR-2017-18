3814ef2e815e3c06653838ffca1435fe0277423d
Max Filip Uršič <maxfilip98@gmail.com>
2017-12-17 17:08:55 +0100

    Popravek pri HTML uvozu

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,22 +1,26 @@
-#link <- "http://ec.europa.eu/eurostat/tgm/table.do?tab=table&init=1&plugin=1&language=en&pcode=tec00118"
-#stran <- html_session(link) %>% read_html()
-#inflacija <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>% .[[1]] %>% html_table(dec = ",")
-#colnames(inflacija) <- c("cas", "drzava")
+link1 <- "http://ec.europa.eu/eurostat/tgm/table.do?tab=table&init=1&plugin=1&language=en&pcode=tec00118"
+stran <- html_session(link1) %>% read_html()
+inflacija <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>% .[[1]] %>% html_table(dec = ",")
+for (i in 1:ncol(inflacija)) {
+  if (is.character(inflacija[[i]])) {
+    Encoding(top4[[i]]) <- "UTF-8"
+  }
+}
+colnames(inflacija) <- c("cas", "drzava")
 
   
   
 
-link <- "https://www.ecb.europa.eu/stats/policy_and_exchange_rates/key_ecb_interest_rates/html/index.en.html"
-stran <- html_session(link) %>% read_html()
-obrestne_mere <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>% .[[0]] %>% html_table(dec = ".")
+link2 <- "https://www.ecb.europa.eu/stats/policy_and_exchange_rates/key_ecb_interest_rates/html/index.en.html"
+stran <- html_session(link2) %>% read_html()
+obrestne_mere <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>% .[[1]] %>% html_table(dec = ".")
 return(obrestne_mere)
-  for (i in 1:ncol(tabela)) {
-    if (is.character(tabela[[i]])) {
-      Encoding(tabela[[i]]) <- "UTF-8"
+  for (i in 1:ncol(obrestne_mere)) {
+    if (is.character(obrestne_mere[[i]])) {
+      Encoding(obrestne_mere[[i]]) <- "UTF-8"
   }
 }
   
-obrestne_mere <- uvozi.obrestne_mere()
 
 
 
