b2aafc995e10ef750b04eaad27ad4b2b24ef7038
Max Filip Uršič <maxfilip98@gmail.com>
2018-01-07 17:27:32 +0100

    dodal 2 grafa

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -5,16 +5,15 @@ zemljevid <- uvozi.zemljevid("http://www.naturalearthdata.com/http//www.naturale
 
 zemljevid <- zemljevid %>% filter (CONTINENT %in% c("Europe"), NAME_LONG != "Russian Federation")
 
-
 graf.BDP <- ggplot() + 
   geom_polygon(data = left_join(zemljevid, BDP %>% filter(leta==2016, sestava=="Gross domestic product at market prices"), by = c("NAME_LONG" = "drzava")),
-               aes(x = long, y = lat, group = group, fill = -vrednost)) +
+               aes(x = long, y = lat, group = group, fill = vrednost)) +
   ggtitle("BDP 2016") + xlab("") + ylab("") +
   guides(fill = guide_colorbar(title = "BDP v eur"))
 
 graf.inflacija <- ggplot() + 
   geom_polygon(data = left_join(zemljevid, inflacija %>% filter(leto==2016), by = c("NAME_LONG" = "drzava")),
-               aes(x = long, y = lat, group = group, fill = -stopnja)) +
+               aes(x = long, y = lat, group = group, fill = stopnja)) +
   ggtitle("inflacija 2016") + xlab("") + ylab("") +
   guides(fill = guide_colorbar(title = "stopnja"))
 
@@ -25,6 +24,11 @@ graf.inflacijaEU <- ggplot(inflacija %>% filter(drzava == "European Union (chang
 graf.BDP2016 <- ggplot(BDP %>% filter(leta == 2016, sestava == "Gross domestic product at market prices", drzava != c("European Union", "Euro area")),
                            aes(x=drzava, y=vrednost)) + geom_col(color="green")
                            
+graf.obrestne <- ggplot(obrestne_mere %>% filter(vrsta == "deposit_facility"),
+                       aes(x=datum, y=vrednost)) + geom_line(color="blue")
+
+graf.rastBDP_Nem <- ggplot(BDP %>% filter(drzava == "Germany", sestava == "Gross domestic product at market prices"),
+                           aes(x=leta, y=vrednost)) + geom_line(color="yellow")
 
 
 
