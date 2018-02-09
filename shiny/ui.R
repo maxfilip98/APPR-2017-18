8eb5fc98f7359ce2e6b8884e724db7a67f495a75
Max Filip Uršič <maxfilip98@gmail.com>
2018-02-09 12:41:09 +0100

    popravek pri grafih Shiny

--- shiny/ui.R
+++ shiny/ui.R
@@ -1,23 +1,23 @@
 library(shiny)
 
 fluidPage(
+  titlePanel("primerjava"),
+  
   tabPanel("Graf",
-           
         sidebarPanel(
-          selectInput("sestava", label = NULL, 
+          selectInput("sestava", label = "Izberi sestavo", 
                       choices = unique(BDP$sestava)),
-          selectInput("drzava", label = NULL, choices=unique(BDP$drzava)),
+          selectInput("drzava", label = "Izberi državo", choices=unique(BDP$drzava))
         ),
-          mainPanel(plotOutput("grafi1"))
+          mainPanel(plotOutput("grafi1"))),
   
   
   tabPanel("Graf",
-                   
         sidebarPanel(
-          selectInput("drzava", label = NULL, 
+          selectInput("drzava", label = "Izberi državo", 
                       choices = unique(inflacija$drzava))
         ),
-          mainPanel(plotOutput("grafi2"))
+          mainPanel(plotOutput("grafi2")))
   
   
 )
